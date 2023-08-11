236b4de2d7f0ad5777f1acfb11528aa1a1df416f
youz2me <kynhun20@gachon.ac.kr>
2023-08-12 01:57:16 +0900

    Build: 프로젝트 초기 설정

---
+++ GachonRookie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GachonRookie
+//
+//  Created by YOUJIM on 2023/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
