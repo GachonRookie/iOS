7b60c1fbbb838198a6ccd7a868d13fb356a944ec
youz2me <kynhun20@gachon.ac.kr>
2023-08-13 21:28:01 +0900

    Build: 폰트 테스트 코드 삭제

--- GachonRookie/Sources/Screen/ViewController.swift
+++ GachonRookie/Sources/Screen/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .darkGray
-        
-        for family in UIFont.familyNames {
-          print(family)
-
-          for sub in UIFont.fontNames(forFamilyName: family) {
-            print("====> \(sub)")
-          }
-        }
     }
 
 }
