3017295b9e348a451c0f306da6bbc9bc3d03eb07
youz2me <kynhun20@gachon.ac.kr>
2023-08-18 13:34:19 +0900

    Build: 익스텐션 및 템플릿 파일 추가

--- GachonRookie/Sources/Screen/ViewController.swift
+++ GachonRookie/Sources/Screen/ViewController.swift
@@ -5,15 +5,63 @@
 //  Created by YOUJIM on 2023/08/12.
 //
 
+// MARK: - ViewController 만들 때마다 템플릿으로 사용할 파일
+// 아래 내용 복붙해서 사용
+
+import SwiftUI
 import UIKit
+
+import SnapKit
+import Then
+
 
 class ViewController: UIViewController {
 
+    // MARK: Variables
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .darkGray
-    }
 
+        setUpView()
+        setUpLayout()
+        setUpDelegate()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    
+    func setUpView() {
+        
+    }
+    
+    
+    // MARK: Layout
+    
+    func setUpLayout() {
+        [
+            
+        ].forEach { view.addSubview($0) }
+    }
+    
+    // MARK: Delegate
+    
+    func setUpDelegate() {
+        
+    }
+    
+    
+    // MARK: Constraint
+    
+    func setUpConstraint() {
+        
+    }
+    
 }
 
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ViewController().toPreview()
+            // .edgesIgnoringSafeArea(.all)
+    }
+}
